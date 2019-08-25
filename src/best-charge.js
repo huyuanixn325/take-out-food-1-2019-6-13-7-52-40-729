bcb3eb9c44dcf10be56f8b72aa85e532939de28e
huyuanixn325 <huyuanxin0325@163.com>
2019-08-25 23:08:49 +0800

    finish countItems

--- src/best-charge.js
+++ src/best-charge.js
@@ -1,3 +1,13 @@
 function bestCharge(selectedItems) {
   return /*TODO*/;
 }
+function countItems(selectedItems) {
+  let result = [];
+  for (let i = 0; i < selectedItems.length; i++) {
+    result.push({
+      id: selectedItems[i].substr(0, 8),
+      count: selectedItems[i].charAt(selectedItems[i].length - 1)
+    });
+  }
+  return result;
+}
