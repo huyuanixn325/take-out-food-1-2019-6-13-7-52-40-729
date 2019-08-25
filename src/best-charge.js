48a4e004d3f77b8f555d3abef5b000084a02f3c1
huyuanixn325 <huyuanxin0325@163.com>
2019-08-25 23:12:10 +0800

    calculate

--- src/best-charge.js
+++ src/best-charge.js
@@ -28,3 +28,11 @@ function getItems(countArray) {
   }
   return items;
 }
+
+function calculate(items) {
+  let totalPrice=0;
+  for (let i = 0; i < items.length; i++) {
+    totalPrice+= items[i].count * items[i].price;
+  }
+  return totalPrice;
+}
